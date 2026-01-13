cfd41269c5f7ed93478d9d9646d2775ecd5b538e
José Abraham Molina Reyes <jamreyes26@gmail.com>
2026-01-13 10:53:56 -0600

    fix: Update ALLOWED_HOSTS to include localhost, 127.0.0.1, and .onrender.com

--- backend/portafolio_backend/settings.py
+++ backend/portafolio_backend/settings.py
@@ -18,7 +18,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 
 # Application definition
